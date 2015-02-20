93771c53344531d0fc55fa6c2c734ff4e57d5ff7
Ravi Kadaboina <ravikiran438@gmail.com>
2015-02-19 23:03:01 -0500

    some more fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,33 +10,6 @@ class User < ActiveRecord::Base
     #before_create :create_referral_code
     #after_create :send_welcome_email
 
-	REFERRAL_STEPS = [
-        {
-            'count' => 5,
-            "html" => "Shave<br>Cream",
-            "class" => "two",
-            "image" =>  ActionController::Base.helpers.asset_path("refer/cream-tooltip@2x.png", :digest => false)
-        },
-        {
-            'count' => 10,
-            "html" => "Truman Handle<br>w/ Blade",
-            "class" => "three",
-            "image" => ActionController::Base.helpers.asset_path("refer/truman@2x.png", :digest => false)
-        },
-        {
-            'count' => 25,
-            "html" => "Winston<br>Shave Set",
-            "class" => "four",
-            "image" => ActionController::Base.helpers.asset_path("refer/winston@2x.png", :digest => false)
-        },
-        {
-            'count' => 50,
-            "html" => "One Year<br>Free Blades",
-            "class" => "five",
-            "image" => ActionController::Base.helpers.asset_path("refer/blade-explain@2x.png", :digest => false)
-        }
-    ]
-
     private
 
     def create_referral_code
